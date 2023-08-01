769b93f17d096c6a87d6050621278df578ca866b
Kien Pham <kien@playstudios.asia>
2023-08-01 17:03:41 +0700

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Button btn20;
+    public Button btn50;
+    public Button btn100;
+
+    public GameObject[] prefab;
+
+    public TextMeshProUGUI fpsText;
+    private float deltaTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn20.onClick.AddListener(() => Spawn(20));
+        btn50.onClick.AddListener(() => Spawn(50));
+        btn100.onClick.AddListener(() => Spawn(100));
+
+        StartCoroutine(CountFps());
+
+    }
+
+
+    private void Spawn(int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            StartCoroutine(WaitSpawn((i)*0.05f +Random.Range(0, 1)));
+        }
+    }
+
+    IEnumerator CountFps()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = Mathf.Ceil(fps).ToString();
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(CountFps());
+    }
+
+    IEnumerator WaitSpawn(float time)
+    {
+        yield return new WaitForSeconds(time);
+        int ran = Random.Range(0, 5);
+        Instantiate(prefab[ran], transform.position, Quaternion.identity);
+
+    }
+
+}
---
+++ Assets/Scripts/MonterController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonterController : MonoBehaviour
+{
+    private Transform wayPoint;
+
+    private List<Transform> currentWaypoint;
+
+    public float speed = 1f;
+
+    private Transform nextPoint;
+    private int curIndex;
+    private int maxPoint;
+
+    private Animator animator;
+    private int animDir = 0;
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetInteger("dir", animDir);
+        int ran = Random.Range(1, 4);
+        wayPoint = GameObject.FindWithTag("WayPoint_" + ran.ToString()).transform;
+
+        transform.position = wayPoint.gameObject.transform.GetChild(0).position;
+        maxPoint = wayPoint.gameObject.transform.childCount;
+        curIndex = 1;
+        nextPoint = wayPoint.gameObject.transform.GetChild(1);
+  
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+            nextPoint.transform.position,
+            speed * Time.deltaTime);
+        if (transform.position.x == nextPoint.position.x && transform.position.y == nextPoint.position.y)
+        {
+            if (curIndex < maxPoint - 1)
+            {
+                curIndex++;
+                nextPoint = wayPoint.gameObject.transform.GetChild(curIndex);
+
+                checkDir(wayPoint.gameObject.transform.GetChild(curIndex - 1), nextPoint);
+
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+    private void checkDir(Transform p1, Transform p2)
+    {
+        if (p1.position.y == p2.position.y) 
+        {
+            if (p2.position.x >= p1.position.x)
+            {
+                animDir = 2;
+            }
+            else
+            {
+                animDir = 3;
+
+            }
+
+        }
+        else if (p1.position.x == p2.position.x)
+        {
+            if (p2.position.y >= p1.position.y)
+            {
+                animDir = 0;
+            }
+            else
+            {
+                animDir = 1;
+
+            }
+        }
+        Debug.Log(animDir);
+        animator.SetInteger("dir", animDir);
+
+    }
+
+}
